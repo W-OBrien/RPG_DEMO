466cb2c2271c76bcbec8b7f8f0a6983ad0e454e5
William <willob@live.ca>
2022-10-15 05:04:10 -0400

    Revert "Base Setup"
    This reverts commit 68a8aed1c4947d0adf9347a64de6126287123286.

--- RPG_DEMO/Source/RPG_DEMO/RPG_DEMO.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RPG_DEMO.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RPG_DEMO, "RPG_DEMO" );
--- RPG_DEMO/Source/RPG_DEMO/RPG_DEMOGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "RPG_DEMOGameModeBase.h"
-
